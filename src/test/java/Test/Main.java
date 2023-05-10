48d243f7e34c05db2ded7ed05abfb8e027579182
lokesh0103 <reachoutlokesh@gmail.com>
2023-05-10 13:03:35 +0530

    Login Test Case Added

--- src/test/java/Test/Main.java
+++ src/test/java/Test/Main.java
@@ -1,4 +1,13 @@
 package Test;
 
+import org.testng.annotations.Test;
+
 public class Main {
+    @Test
+    public static void login() throws InterruptedException {
+
+        PostLogin.Login();
+
+    }
+
 }
