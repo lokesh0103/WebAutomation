ffc41f11590217b09c6fc9ed3b7dcd5f92110af2
lokesh0103 <reachoutlokesh@gmail.com>
2023-05-10 15:54:50 +0530

    Removed Unused code

--- src/test/java/Test/Main.java
+++ src/test/java/Test/Main.java
@@ -1,8 +1,16 @@
 package Test;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Main {
+
+    @BeforeClass
+    public static void setUp(){
+
+        System.out.println("Setting up Drivers & Environment");
+    }
+
     @Test
     public static void login() throws InterruptedException {
 
