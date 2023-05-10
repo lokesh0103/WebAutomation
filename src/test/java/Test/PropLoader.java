67b11215cc711c0babec7b054ffd929b4d9479c8
lokesh0103 <reachoutlokesh@gmail.com>
2023-05-10 12:16:08 +0530

    Handled NullPointerException

--- src/test/java/Test/PropLoader.java
+++ src/test/java/Test/PropLoader.java
@@ -1,4 +1,27 @@
 package Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class PropLoader {
+
+    public static Properties loadProperties() throws IOException {
+
+        Properties properties = new Properties();
+
+        try (FileInputStream input = new FileInputStream("D:\\Intellij idea\\WebAutomation\\src\\test\\java\\General.properties")) {
+            properties.load(input);
+
+        } catch (IOException e) {
+            System.out.println(e.toString());
+        }
+
+        if(properties.isEmpty()){
+
+            throw new NullPointerException("Failed to load properties");
+        }
+        return properties;
+    }
+
 }
